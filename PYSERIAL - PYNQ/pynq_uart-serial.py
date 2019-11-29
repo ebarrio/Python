d6899bae0d69e3a1554b6685391399b7e998d047
Eladio Barrio Querol <47576771+ebarrio@users.noreply.github.com>
2019-11-29 21:21:09 +0100

    Add files via upload

---
+++ PYSERIAL - PYNQ/pynq_uart-serial.py
@@ -0,0 +1,26 @@
+
+try:
+    import serial
+except ImportError:
+    print('Pyserial s not installed.')
+    raise
+import time
+import threading
+
+SERIAL_MSG_BYTES = 3
+SERIAL_PORT = 'COM11'
+SERIAL_BAUD = 115200
+
+def serial_read_thread():
+    print('Hello')
+    if(zynq_uart.inWaiting()>0):
+        msg_read = zynq_uart.read(SERIAL_MSG_BYTES)
+        print(msg_read)
+
+if __name__ == "__main__" :
+    zynq_uart = serial.Serial(SERIAL_PORT,SERIAL_BAUD)
+    time.sleep(1)
+    th_1 = threading.Timer(0.5 , serial_read_thread)
+    th_1.start()
+    time.sleep(10)
+    #zynq_uart.close()
