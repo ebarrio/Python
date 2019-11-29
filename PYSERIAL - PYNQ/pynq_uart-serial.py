3aa5226738e8b6a9199a915bb58b3d1bb4e0f17a
Eladio Barrio Querol <47576771+ebarrio@users.noreply.github.com>
2019-11-29 21:27:20 +0100

    Add files via upload

--- PYSERIAL - PYNQ/pynq_uart-serial.py
+++ PYSERIAL - PYNQ/pynq_uart-serial.py
@@ -2,7 +2,7 @@
 try:
     import serial
 except ImportError:
-    print('Pyserial is not installed.')
+    print('Pyserial s not installed.')
     raise
 import time
 import threading
@@ -12,15 +12,17 @@ SERIAL_PORT = 'COM11'
 SERIAL_BAUD = 115200
 
 def serial_read_thread():
-    print('Hello')
-    if(zynq_uart.inWaiting()>0):
-        msg_read = zynq_uart.read(SERIAL_MSG_BYTES)
-        print(msg_read)
+    while(True):
+        if(zynq_uart.inWaiting()>0):
+            msg_read = zynq_uart.read(SERIAL_MSG_BYTES)
+            print(msg_read)
+        time.sleep(0.5)
 
 if __name__ == "__main__" :
     zynq_uart = serial.Serial(SERIAL_PORT,SERIAL_BAUD)
     time.sleep(1)
-    th_1 = threading.Timer(0.5 , serial_read_thread)
+    th_1 = threading.Thread(target = serial_read_thread)
     th_1.start()
     time.sleep(10)
     zynq_uart.close()
+    raise Exception('Program finish')
